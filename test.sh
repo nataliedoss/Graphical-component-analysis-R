3602ee2e9edaaea68839e666f54714b958c87416
Natalie Doss <31667433+nataliedoss@users.noreply.github.com>
2020-01-17 15:33:54 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=day
-#SBATCH --job-name=test
-#SBATCH --cpus-per-task=6
-#SBATCH --time=24:00:00
-#SBATCH --mail-user=natalie.doss@yale.edu
-
-module load R
-Rscript test.r
